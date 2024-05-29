c446dc5d8eb0d700403e09763342aa3d686f7021
yw819 <yifei_wan@dfci.harvard.edu>
2024-05-28 23:34:30 -0400

    BXMD-191/BW-132/BW-134 Feat: Handle selected track file URL modified:   js/loadTracksFromServerWidget.js
    Handle the selected track file URL when the user clicks the "OK" button in the track selection modal to pass the track file URL to the trackLoadHandler().

--- js/loadTracksFromServerWidget.js
+++ js/loadTracksFromServerWidget.js
@@ -1,5 +1,4 @@
-// Add a new button(s) to load track file(s) from the server /dist/*
-import { FileLoadManager, MultipleTrackFileLoad, createGenericSelectModal } from '../node_modules/igv-widgets/dist/igv-widgets.js'
+let selectedFileUrl = '';
 
 const createTrackServerModal = id => {
 
@@ -41,46 +40,116 @@ const createTrackServerModal = id => {
 
 }; 
 
-// Function to fetch the list of files
 window.fetchFileList = async function(path = '') {
-    fetch(`/dist${path}`)
-    .then(response => response.text())
-    .then(data => {
-      const parser = new DOMParser();
-      const htmlDoc = parser.parseFromString(data, 'text/html');
-      const files = htmlDoc.querySelectorAll('a');
-      const fileList = document.getElementById('fileList');
-      fileList.innerHTML = '';
+    console.info("Fetch input path: ", path);
+    try {
+        if (path === '') {
+            path = `/dist${path}`;
+        };
+        console.info("Current work diretory: ", path);
+        const response = await fetch(path);
+        const data = await response.text();
+        const parser = new DOMParser();
+        const htmlDoc = parser.parseFromString(data, 'text/html');
+        const files = htmlDoc.querySelectorAll('a');
+        const fileList = document.getElementById('fileList');
+        fileList.innerHTML = '';
 
-      const goBackButton = document.getElementById('goBackButton');
-      if (path) {
-        // Show the "Go Back" button if not in the root directory
-        goBackButton.style.display = 'block';
-        goBackButton.setAttribute('data-path', path);
-      } else {
-        // Hide the "Go Back" button if in the root directory
-        goBackButton.style.display = 'none';
-      }
+        const goBackButton = document.getElementById('goBackButton');
+        if (path !== '/dist') {
+            // Show the "Go Back" button if not in the root directory
+            goBackButton.style.display = 'block';
+            goBackButton.setAttribute('data-path', path);
+        } else {
+            // Hide the "Go Back" button if in the root directory
+            goBackButton.style.display = 'none';
+        }
 
-      files.forEach(file => {
-        const li = document.createElement('li');
-        const filePath = path + '/' + file.textContent.trim();
-        li.innerHTML = `<a href="#" onclick="fetchFileList('${filePath}'); return false;">${file.textContent}</a>`;
-        fileList.appendChild(li);
-      });
-    })
-    .catch(error => console.error('Error fetching file list:', error));
+        files.forEach(file => {
+            const li = document.createElement('li');
+            const filePath = path + '/' + file.textContent.trim();
+            li.innerHTML = `<a href="#" data-url="${filePath}" onclick="selectFile('${filePath}'); return false;">${file.textContent}</a>`;
+            fileList.appendChild(li);
+        });
+    } catch (error) {
+        console.error('Error fetching file list:', error);
+    }
 }
 
 // Function to navigate back to the parent directory
 window.goBack = function() {
     const goBackButton = document.getElementById('goBackButton');
     const currentPath = goBackButton.getAttribute('data-path');
+    console.info("Current path: ", currentPath);
     const parentPath = currentPath.split('/').slice(0, -1).join('/');
+    console.info("Parient path: ", parentPath);
     fetchFileList(parentPath);
-  }
+}
 
-function loadTracksFromServerWithRegistry (
+// Function to check the path is file or directory
+async function isFileOrDirPath(filePath) {
+    try {
+        const response = await fetch(`/isFileOrDirPath?filePath=${encodeURIComponent(filePath)}`);
+        const data = await response.json();
+        return data.type; // Return the type ('file' or 'dir')
+    } catch (error) {
+        console.error('Error checking file or directory:', error);
+        throw error; // Handle the error as needed
+    }
+}
+
+window.selectFile = async function(filePath) {
+        // TODO: Add multiple tracks support
+        selectedFileUrl = ''; // clear filePath 
+        try {
+            const type = await isFileOrDirPath(filePath);
+            if (type === 'dir') {
+                // If it's a directory, fetch and display its contents
+                fetchFileList(filePath);
+            } else if (type === 'file') {
+                // If it's a file, set it as the selected file
+               selectedFileUrl = filePath;
+                // Visually indicate the selected file, e.g., by adding a class
+                const fileLinks = document.querySelectorAll('#fileList a');
+                fileLinks.forEach(link => {
+                    if (link.getAttribute('data-url') === filePath) {
+                        link.classList.add('selected'); // Add a class to the selected link
+                    } else {
+                        link.classList.remove('selected'); // Remove the class from non-selected links
+                    }
+                });
+            } else {
+                console.error('Invalid type:', filePath);
+                console.error('Invalid type:', type);
+            }
+        } catch (error) {
+            console.error('Error selecting file:', error);
+        }
+    }
+
+function handleOkClick(trackLoadHandler) {
+    return function() {
+        console.info('Selected Track file URL:', selectedFileUrl);
+        const configurations = [];
+        if (selectedFileUrl) {
+            configurations.push({"url": selectedFileUrl});
+            // Clear the selectedFileUrl and remove 'selected' class
+            selectedFileUrl = '';
+            const fileLinks = document.querySelectorAll('#fileList a.selected');
+            fileLinks.forEach(link => {
+                link.classList.remove('selected');
+            });
+            // Load track file(s)
+            if (configurations.length > 0) {
+                trackLoadHandler(configurations);
+            }
+        } else {
+            console.log('No files selected.');
+        }
+    }
+}
+
+function loadTracksFromServerWithRegistry(
     $igvMain,
     $dropdownMenu,
     $localFileInput,
@@ -98,90 +167,11 @@ function loadTracksFromServerWithRegistry (
     const trackFromServerModal = createTrackServerModal(trackFromServerModalId);
     fetchFileList();
     $igvMain.get(0).appendChild(trackFromServerModal);
-    //populateSelect("select-track-file-from-server", "http://localhost:8080/tests/"); 
 
-    /*
-    const fileLoadWidgetConfig = {
-        widgetParent: urlModal.querySelector('.modal-body'),
-        dataTitle: 'Track',
-        indexTitle: 'Index',
-        mode: 'url',
-        fileLoadManager: new FileLoadManager(),
-        dataOnly: false,
-        doURL: false 
-    };
+    const okHandler = handleOkClick(trackLoadHandler);
 
-    //TODO: Check and change
-    const fileLoadWidget = new FileLoadWidget(fileLoadWidgetConfig);
-
-    configureModal(fileLoadWidget, urlModal, async (fileLoadWidget) => {
-        const paths = fileLoadWidget.retrievePaths();
-        await multipleTrackFileLoad.loadPaths(paths);
-        return true;
-    });
-    */
-
-    /*
-    const multipleTrackFileLoadConfig = {
-        $localFileInput,
-        initializeDropbox,
-        $dropboxButton,
-        $googleDriveButton: googleEnabled ? $googleDriveButton : undefined,
-        fileLoadHandler: trackLoadHandler,
-        multipleFileSelection: true
-    };
-
-    const multipleTrackFileLoad = new MultipleTrackFileLoad(multipleTrackFileLoadConfig);
-    */
-
-/*    const encodeModalTables = [];
-    for (let modalID of encodeTrackModalIds) {
-        const encodeModalTableConfig = {
-            id: modalID,
-            title: 'ENCODE',
-            selectionStyle: 'multi',
-            pageLength: 100,
-            okHandler: trackLoadHandler
-        };
-
-        encodeModalTables.push(new ModalTable(encodeModalTableConfig));
-    }
-
-    if (selectModalIdOrUndefined) {
-        const $genericSelectModal = $(createGenericSelectModal(selectModalIdOrUndefined, `${selectModalIdOrUndefined}-select`));
-        $igvMain.append($genericSelectModal);
-
-        const $select = $genericSelectModal.find('select');
-        const $dismiss = $genericSelectModal.find('.modal-footer button:nth-child(1)');
-        const $ok = $genericSelectModal.find('.modal-footer button:nth-child(2)');
-
-        $dismiss.on('click', () => $genericSelectModal.modal('hide'));
-
-        const okHandler = () => {
-            const configurations = [];
-            const $selectedOptions = $select.find('option:selected');
-
-            $selectedOptions.each(function () {
-                configurations.push($(this).data('track'));
-                $(this).removeAttr('selected');
-            });
-
-            if (configurations.length > 0) {
-                trackLoadHandler(configurations);
-            }
-
-            $genericSelectModal.modal('hide');
-        };
-
-        $ok.on('click', okHandler);
-
-        $genericSelectModal.get(0).addEventListener('keypress', (event) => {
-            if (event.key === 'Enter') {
-                okHandler();
-            }
-        });
-    }
-    */
+    const $ok = $('.modal-footer').find('.btn.btn-sm.btn-secondary[data-dismiss="modal"]');
+    $ok.on('click', okHandler);
 }
 
-export {loadTracksFromServerWithRegistry};
+export { loadTracksFromServerWithRegistry };
